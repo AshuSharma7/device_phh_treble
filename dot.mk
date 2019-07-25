2e6fb8d2ce73bd22da24f3e627e3ecb05f3f4075
Ashusharma7 <ashu7073657271@gmail.com>
2019-07-25 12:02:28 +0530

    Initial commit

--- dot.mk
+++
@@ -1,4 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
---
+++ treble_arm64_avN/AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/dot_treble_arm64_avN.mk
